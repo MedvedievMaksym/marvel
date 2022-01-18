adf4858a8755bc26a5ba82271644fc5f0d30a7fb
MedvedievM <medvediev.m91@gmail.com>
2022-01-18 16:35:45 +0200

    create pages and navigation using react router

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -5,12 +5,12 @@ export const useHttp = () => {
 	const [ loading, setLoading ] = useState(false);
 	const [ error, setError ] = useState(null);
 
-	const request = useCallback(async (url, method = 'GET', body = null,
-	                                   headers = {'Content_Type': 'application/json'}) => {
+	const request = useCallback(async (url, method = 'GET', body = null, mode: "no-cors",
+	                                   headers = {'Content-Type': 'application/json'}) => {
 		setLoading(true);
 
 		try {
-			const response = await fetch(url, {method, body, headers});
+			const response = await fetch(url, {method, body, headers, mode});
 			if(!response.ok) {
 				throw new Error(`Could you fetch ${url}, status ${response.status}`);
 			}
